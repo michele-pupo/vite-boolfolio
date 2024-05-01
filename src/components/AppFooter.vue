e0fdaefe8d73c6b2bf2c65ed192064cec9f44fa6
pool.gutierrez <pool.gutierrezv@gmail.com>
2024-05-01 00:15:05 -0500

    added header e footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,71 @@
+<script>
+
+export default {
+    name: 'AppFooter',
+}
+
+</script>
+
+
+<template>
+
+<footer>
+
+    <div class="container">
+        <div class="top-footer d-flex justify-content-around py-5">
+            <div class="links">
+                <h5>LINKS</h5>
+                <ul>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                </ul>
+            </div>
+            <div class="links">
+                <h5>LINKS</h5>
+                <ul>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                </ul>
+            </div>
+            <div class="links">
+                <h5>LINKS</h5>
+                <ul>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="bottom-footer text-center">
+            <p>
+                <small>Copyright (c): 2024 Boolean Classe #188 - Team 9</small>
+            </p>
+        </div>
+    </div>
+
+</footer>
+
+</template>
+
+
+<style lang="scss">
+
+footer {
+    border-top: 1px solid white;
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+}
+
+</style>
