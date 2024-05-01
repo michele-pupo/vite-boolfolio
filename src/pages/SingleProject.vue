132b4d485c0b4991406f5fe17663dfdcae2b0ccb
michele-fabio-pupo <mikypupo@hotmail.it>
2024-05-01 07:56:51 +0200

    printed in SingleProject title and description

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,11 +1,26 @@
 <script>
+
+import axios from 'axios';
+
 export default{
-    name:'SinglePost',
+    name:'SingleProject',
 
     data(){
         return{
+            project:null,
 
+            apiBaseUrl: 'http://127.0.0.1:8000/api/'
         }
+    },
+
+    mounted(){
+        axios.get(this.apiBaseUrl + 'projects/1').then(res =>{
+
+            console.log(res)
+
+            this.project = res.data.result
+
+        })
     }
 }
 
@@ -15,10 +30,18 @@ export default{
 
 <template>
 
-    <div>
+    <div class="text-center" v-if="project">
         <h2>
-            Pagina del project singolo
+            {{ project.name }}
         </h2>
+        <p>
+            {{ project.description }}
+        </p>
+    </div>
+    <div v-else>
+        <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+        </div>
     </div>
 
 </template>
