e03221f693f871420c77f359444627463f092001
simoneditaranto <simodita99@gmail.com>
2024-05-02 10:57:09 +0200

    feat: crated pagination and loader

--- src/App.vue
+++ src/App.vue
@@ -18,24 +18,57 @@ export default {
             // link ai vari endpoint dell'api per vedere le varie pagine dei post
             apiLinks: [],
 
+            // variabile che tiene traccia della pagina corrente
+            // per la chiamata api
+            apiPageNumber: 1,
+
+            // variabile per il loader
+            isLoading: true,
+
             baseApiUrl: 'http://127.0.0.1:8000/api',
         }
     },
 
     mounted() {
 
-        axios.get(this.baseApiUrl + '/projects').then(res => {
+      this.apiCall();  
 
-            // console.log(res.data.result.links);
+    },
+
+    methods: {
+
+      apiCall() {
+         // implementiamo il loader anche qui
+         // impostiamo la variabile isLoading a true
+         this.isLoading = true;
+
+         axios.get(this.baseApiUrl + '/projects', {
+         params: {
+            page: this.apiPageNumber, 
+         } 
+        }).then(res => {
+
+         // solo quando riceviamo una risposta di successo
+         if(res.data.success) {
+            // impostiamo isLoading a false
+            this.isLoading = false;
+         }
+
+            // console.log(res);
 
             this.projects = res.data.result.data;
 
             this.apiLinks = res.data.result.links;
 
         })
-    },
+      },
 
-    methods: {
+      changeApiPage(pageNumber) {
+         // console.log(pageNumber);
+         this.apiPageNumber = pageNumber;
+
+         this.apiCall();
+      },
 
     },
 }
@@ -45,27 +78,37 @@ export default {
 
    <!-- <AppMain></AppMain> -->
    <div class="container">
-      <div class="projects d-flex justify-content-between">
-         <AppProject 
-            v-for="currentProject in projects"
-            :projectName="currentProject.name"
-            :projectDescription="currentProject.description"
-            :projectImage="currentProject.project_image"
-            :projectDate="currentProject.project_date"
-            :projectLink="currentProject.link_github"
-            :projectTechnolgies="currentProject.technologies"
-            :projectType="currentProject.type.title"
-         >
-         </AppProject>
+
+      <div v-if="!isLoading">
+
+         <div class="projects d-flex justify-content-between">
+            <AppProject 
+               v-for="currentProject in projects"
+               :projectName="currentProject.name"
+               :projectDescription="currentProject.description"
+               :projectImage="currentProject.project_image"
+               :projectDate="currentProject.project_date"
+               :projectLink="currentProject.link_github"
+               :projectTechnolgies="currentProject.technologies"
+               :projectType="currentProject.type.title"
+            >
+            </AppProject>
+         </div>
+
+         <div class="pages">
+            <ul class="">
+               <li v-for="link in apiLinks" v-html="link.label" @click="changeApiPage(link.label)" :class="link.label == apiPageNumber ? 'active' : ''"></li>
+            </ul>
+         </div>
+         
+      </div>
+      <div v-else>
+         <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+         </div>
       </div>
 
-      <div class="pages">
-         <ul class="d-flex gap-3">
-            <li v-for="link in apiLinks">
-               <a href="" v-html="link.label"></a>
-            </li>
-         </ul>
-      </div>
+
 
    </div>
 
@@ -76,7 +119,22 @@ export default {
 
 .pages {
    ul {
+      display: flex;
+      gap: 10px;
+
       list-style-type: none;
+
+      li {
+         padding: 8px;
+
+         transition: all .3s ease;
+
+         cursor: pointer;
+
+         &:hover, &.active{
+            background-color: rgba(255, 255, 255, 0.4);
+         }
+      }
    }
 }
 
