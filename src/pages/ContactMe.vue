c63aa77d50ccf3635d3240bd5dd308d28dd7ee80
simoneditaranto <simodita99@gmail.com>
2024-05-03 12:33:20 +0200

    feat: contact-me form created

--- src/pages/ContactMe.vue
+++ src/pages/ContactMe.vue
@@ -1,11 +1,23 @@
 <script>
 
+import ContactForm from '../components/ContactForm.vue';
+
+export default {
+
+    components: {
+        ContactForm,
+    }
+
+}
+
 </script>
 
 <template>
 
     <div class="container">
-        <h1>Contattami</h1>
+        <h1 class="mt-3">Contattami</h1>
+
+        <ContactForm></ContactForm>
     </div>
 
 </template>
